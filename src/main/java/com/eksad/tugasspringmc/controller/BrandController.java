3ba5eda3627b4f9275c03660afeb9c6e062d7104
ridhwankn99 <ridhwankn99@gmail.com>
2019-07-08 19:06:58 +0700

    Second

--- src/main/java/com/eksad/tugasspringmc/controller/BrandController.java
+++ src/main/java/com/eksad/tugasspringmc/controller/BrandController.java
@@ -2,10 +2,13 @@ package com.eksad.tugasspringmc.controller;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,6 +27,8 @@ public class BrandController {
 		
 		model.addAttribute("listBrand", list);
 		return "list";
+		
+		
 	}
 	
 	@RequestMapping("/add")
@@ -40,11 +45,22 @@ public class BrandController {
 		
 		return "redirect:/brand";
 	}
-	@RequestMapping("/edit")
-	public String editBrand(Model model) {
-		Brand brand = new Brand();
-		
+	@RequestMapping("/delete/{id}")
+	public String delete(@PathVariable ("id") Long id) {
+		Brand brand = brandRepositoryDao.getOne(id);
+		if(brand==null)
+		{
+			throw new EntityNotFoundException("Not Found");
+		}
+		brandRepositoryDao.delete(brand);
+		return "redirect:/brand";
+	}	
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editBrand(@PathVariable Long id, Model model) {
+		Brand brand = brandRepositoryDao.findOne(id);
 		model.addAttribute("brand", brand);
-		return "edit";
+		
+		return "add";
 	}
 }
